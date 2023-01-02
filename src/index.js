e353772acc56f1727714fbe6031d87abcb383d92
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-02 17:57:23 +0100

    Add StrictMode

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import { createRoot } from 'react-dom/client';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import App from './App';
+import { StrictMode } from 'react';
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
