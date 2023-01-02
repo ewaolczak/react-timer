b8a3bbe4e5dd3f5842e4df483ff5a1c4e702a042
Ewa Olczak <ewa.olczak@gmail.com>
2023-01-02 17:44:19 +0100

    Add styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import './styles/normalize.scss';
+import './styles/global.scss';
 import App from './App';
 
 const container = document.querySelector('#root');
